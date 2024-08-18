7324cbafd3e68742934d6ed76b72421ee579d71e
nestor ivan castro <netorivan2801@gmail.com>
2024-08-18 08:02:33 -0500

    Añadir funciones.js y funtions.html

---
+++ funciones.js
@@ -0,0 +1,16 @@
+// <!-- subsection of imagens and columns -------------------------------------------------- -->
+function img_one_info() {
+    document.getElementById("menu1").innerHTML = "<h1>croasan $1500  </h1>";
+}
+
+function img_one_info2() {
+    document.getElementById("menu2").innerHTML = "<p>un poco de diversidad de nuestro planeta, para mostrar nustro compromiso con el medio ambiente</p>";
+}
+function img_one_info3() {
+    document.getElementById("menu3").innerHTML = "<p>un poco de diversidad de nuestro planeta, para mostrar nustro compromiso con el medio ambiente</p>";
+}
+// fin de  subsections-------------------------------
+// <!-- info de home -->
+function mostrarTexto2() {
+    document.getElementById('info2').innerHTML = 'Este es el texto que se mostrará al hacer clic en la imagen.';
+}
